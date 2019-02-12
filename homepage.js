b32463e5fb9f5a515de7e483b94d053f33cdd01e
delizious <46890900+delizious@users.noreply.github.com>
2019-02-12 22:54:18 +1300

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
 	var image = $('.homepage-top-image');
-	var number = Math.ceil(Math.random()*14);
+	var number = Math.ceil(Math.random()*15);
 	if (number < 10) {
 		number = '0' + number;
 	}
 	image.attr('src', 'img/homepage/main-'.concat(number).concat('.jpg'));
+	image.on('load', function(){image.addClass('show')});
 });
